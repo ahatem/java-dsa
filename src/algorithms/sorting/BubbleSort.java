ca311aed077273f7ee3019cda92ffdd05377af30
Ahmed Hatem <ahatem925@gmail.com>
2025-09-03 02:40:01 +0300

    feat(sorting): add sort order to bubble sort
    - update bubble sort to accept sort order (ascending/descending)
    - introduce Sort.Order enum for explicit sort direction
    - add shouldSwap utility method to handle comparisons based on sort order

--- src/algorithms/sorting/BubbleSort.java
+++ src/algorithms/sorting/BubbleSort.java
@@ -3,12 +3,12 @@ package algorithms.sorting;
 public final class BubbleSort {
     // Space: O(1) in-place
     // Time : O(n^2) worst/average, O(n) best case
-    public static <T extends Comparable<T>> void sort(T[] array) {
+    public static <T extends Comparable<T>> void sort(T[] array, Sort.Order order) {
         for (int i = 0; i < array.length - 1; i++) {
             boolean swapped = false;
             for (int j = 0; j < array.length - 1 - i; j++) {
                 // Compare adjacent elements, bubble largest to end            
-                if (array[j].compareTo(array[j + 1]) > 0) {
+                if (Sort.shouldSwap(array[j], array[j + 1], order)) {
                     Sort.swap(array, j, j + 1);
                     swapped = true;
                 }
--- src/algorithms/sorting/Sort.java
+++ src/algorithms/sorting/Sort.java
@@ -2,9 +2,22 @@ package algorithms.sorting;
 
 // Utility class for shared sorting methods
 public final class Sort {
+    public static enum Order {
+        DESCENDING, ASCENDING
+    }
+
     public static <T> void swap(T[] array, int firstIndex, int secondIndex) {
         T temp = array[firstIndex];
         array[firstIndex] = array[secondIndex];
         array[secondIndex] = temp;
+    }
+
+    public static <T extends Comparable<T>> boolean shouldSwap(T firstItem, T secondItem, Order sortOrder) {
+        return switch (sortOrder) {
+            // swap if the first item is greater than the second
+            case ASCENDING -> firstItem.compareTo(secondItem) > 0;
+            // swap if the first item is less than the second
+            case DESCENDING -> firstItem.compareTo(secondItem) < 0;
+        };
     }
 }
